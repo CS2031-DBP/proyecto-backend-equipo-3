eb0b503fe7f8dbff820274c4e20801d4079bfa37
AndreaCoa <andrea.coa@utec.edu.pe>
2024-04-29 19:09:22 -0500

    test hello world

---
+++ src/main/java/project/petpals/PetPalsApplication.java
@@ -0,0 +1,15 @@
+package project.petpals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPalsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(PetPalsApplication.class, args);
+        System.out.println("Hello World!");
+    }
+
+}
