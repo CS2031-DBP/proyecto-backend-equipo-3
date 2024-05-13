3be9895b41c6318b1b0d32d14689c5b6e7ae3e21
jesus gadiel velarde tipte <jesus.velarde@utec.edu.pe>
2024-05-13 14:29:04 -0500

    test commit

--- src/main/java/project/petpals/activity/dtos/NewActivityDto.java
+++ src/main/java/project/petpals/activity/dtos/NewActivityDto.java
@@ -1,4 +1,35 @@
 package project.petpals.activity.dtos;
 
+import lombok.Data;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import java.time.LocalDateTime;
+
+
+//import jakarta.validation.Valid;
+//import jakarta.validation.constraints.*;
+//import lombok.Data;
+
+
+
+@Data
 public class NewActivityDto {
+    @NotNull
+    private String address;
+    @NotNull
+    private String name;
+    @NotNull
+    private LocalDateTime endDate;
+    @NotNull
+    private LocalDateTime startDate;
+    @NotNull
+    private ActivityType activityType;
+
 }
+
+
+
+
+
+
+
