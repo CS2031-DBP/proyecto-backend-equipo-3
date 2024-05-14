a359e67a7370cdca20c23b5d4aa6a2b14b6dde10
JoaquinHuaman <joaquin.huaman@utec.edu.pe>
2024-05-14 01:31:33 -0500

    Cambios1(14/05)

--- src/main/java/project/petpals/adoption/application/AdoptionController.java
+++ src/main/java/project/petpals/adoption/application/AdoptionController.java
@@ -1,4 +1,27 @@
 package project.petpals.adoption.application;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import project.petpals.adoption.domain.Adoption;
+
 public class AdoptionController {
+
+    @GetMapping("/{adoptionId}")
+    public ResponseEntity<Adoption> getAdoption(@PathVariable Long adoptionId) {
+        return ResponseEntity.ok(adoptionService.getAdoption(adoptionId));
+    }
+
+    @GetMapping("/user{userId}")
+    public ResponseEntity<Adoption> getAdoptionByUser(@PathVariable Long userId) {
+        return ResponseEntity.ok(adoptionService.getAdoptionByUser(userId));
+    }
+
+    @PostMapping()
+    public ResponseEntity<Void> saveAdoption(@RequestBody Adoption adoption) {
+        adoptionService.saveAdoption(adoption);
+        return ResponseEntity.created(null).build();
+    }
 }
--- src/main/java/project/petpals/subscription/application/SubscriptionController.java
+++ src/main/java/project/petpals/subscription/application/SubscriptionController.java
@@ -1,4 +1,30 @@
 package project.petpals.subscription.application;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import project.petpals.subscription.domain.Subscription;
+
 public class SubscriptionController {
+
+    @PostMapping()
+    public ResponseEntity<Void> saveSubscription(@RequestBody Subscription subscription) {
+        subscriptionService.saveSubscription(subscription);
+        return ResponseEntity.created(null).build();
+    }
+
+    @GetMapping("/company/{companyId}")
+    public ResponseEntity<Subscription> getSubscriptionByCompany(@PathVariable Long companyId) {
+        return ResponseEntity.ok(subscriptionService.getSubscriptionByCompany(companyId));
+    }
+
+    @GetMapping("/person/{personId}")
+    public ResponseEntity<Subscription> getSubscriptionByPerson(@PathVariable Long personId) {
+        return ResponseEntity.ok(subscriptionService.getSubscriptionByPerson(personId));
+    }
+
+    @DeleteMapping("/{subscriptionId}")
+    public ResponseEntity<Void> deleteSubscription(@PathVariable Long subscriptionId) {
+        subscriptionService.deleteSubscription(subscriptionId);
+        return ResponseEntity.noContent().build();
+    }
 }
