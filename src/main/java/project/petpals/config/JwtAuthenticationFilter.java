1f22180379bc3ab12ed53a57654996d1592ae96d
AndreaCoa <andrea.coa@utec.edu.pe>
2024-05-17 23:24:28 -0500

    minor changes

--- src/main/java/project/petpals/config/JwtAuthenticationFilter.java
+++ src/main/java/project/petpals/config/JwtAuthenticationFilter.java
@@ -1,4 +1,7 @@
 package project.petpals.config;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class JwtAuthenticationFilter {
 }
--- src/main/java/project/petpals/config/JwtService.java
+++ src/main/java/project/petpals/config/JwtService.java
@@ -1,4 +1,7 @@
 package project.petpals.config;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class JwtService {
 }
