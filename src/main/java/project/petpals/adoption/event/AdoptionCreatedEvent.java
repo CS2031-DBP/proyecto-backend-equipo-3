579595e106e85933dc7afb1929c05881843b3f33
AndreaCoa <andrea.coa@utec.edu.pe>
2024-05-26 14:44:23 -0500

    mail works

--- src/main/java/project/petpals/adoption/event/AdoptionCreatedEvent.java
+++ src/main/java/project/petpals/adoption/event/AdoptionCreatedEvent.java
@@ -1,12 +1,14 @@
 package project.petpals.adoption.event;
 
 
+import org.springframework.context.ApplicationEvent;
 import project.petpals.adoption.domain.Adoption;
 
-public class AdoptionCreatedEvent {
+public class AdoptionCreatedEvent extends ApplicationEvent {
     private final Adoption adoption;
 
     public AdoptionCreatedEvent(Adoption adoption) {
+        super(adoption);
         this.adoption = adoption;
     }
 
--- src/main/java/project/petpals/adoption/event/AdoptionEventListener.java
+++ src/main/java/project/petpals/adoption/event/AdoptionEventListener.java
@@ -3,6 +3,7 @@ package project.petpals.adoption.event;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import project.petpals.adoption.domain.Adoption;
 import project.petpals.adoption.infrastructure.EmailService;
@@ -15,11 +16,13 @@ public class AdoptionEventListener {
     private EmailService emailService;
 
     @EventListener
+    @Async
     public void handleAdoptionCreatedEvent(AdoptionCreatedEvent event) {
         Adoption adoption = event.getAdoption();
         Person person = adoption.getPerson();
         String subject = "Gracias por adoptar a una mascota";
         String text = "Querido " + person.getName() + ",\n\nGracias por adoptar a " + adoption.getPet().getName() + ".\n\nSaludos,\nPetPals";
         emailService.sendSimpleMessage(person.getEmail(), subject, text);
+//        emailService.sendSimpleMessage("andrea.coa@utec.edu.pe", subject, text)
     }
 }
